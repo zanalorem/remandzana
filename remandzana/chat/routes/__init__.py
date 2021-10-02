3994224051e94a282d4a0a252d1d6d361dfd6e77
zanalorem <91803188+zanalorem@users.noreply.github.com>
2021-10-02 20:46:26 +0000

    Create __init__.py

---
+++ remandzana/chat/routes/__init__.py
@@ -0,0 +1,35 @@
+from quart import Blueprint
+
+from ._common import routes as common_routes
+from .two import two
+from .three import three
+from .question import question, question_ask, question_discuss
+from .any import any_
+from .global_ import global_
+
+
+bp = Blueprint("chat", __name__)
+
+
+@bp.after_request
+def cache_control(response):
+    response.cache_control.no_store = True
+    response.cache_control.max_age = 0
+    return response
+
+
+routes = [
+    ("/two", two),
+    ("/three", three),
+    ("/question", question),
+    ("/question/ask", question_ask),
+    ("/question/discuss", question_discuss),
+    ("/any", any_),
+    ("/global", global_)
+]
+
+for r in common_routes:
+    bp.add_url_rule(**r)
+
+for rule, view_func in routes:
+    bp.add_url_rule(rule=rule, view_func=view_func)
