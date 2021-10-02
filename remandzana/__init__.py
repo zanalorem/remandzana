e044af122545fb81b9262fd08bbb66305cf9528f
zanalorem <91803188+zanalorem@users.noreply.github.com>
2021-10-02 20:37:01 +0000

    Create __init__.py

---
+++ remandzana/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+from quart import Quart
+
+
+def create_app():
+    app = Quart(__name__)
+    app.config.from_pyfile("../config.py")
+    app.config["REMANDZANA_FEEDBACK_DIRECTORY"] = os.path.join(
+        app.root_path,
+        app.config["REMANDZANA_FEEDBACK_DIRECTORY"]
+    )
+
+    from .main import bp as main_bp
+    from .chat import bp as chat_bp
+    app.register_blueprint(main_bp)
+    app.register_blueprint(chat_bp)
+
+    from .models.metrics import Metrics
+    app.metrics = Metrics(
+        app.config.get("REMANDZANA_METRICS_PERIOD", 86400),
+        app.config.get("REMANDZANA_METRICS_COOLDOWN", 60)
+    )
+
+    @app.after_request
+    def cache_control(response):
+        content_type = response.headers.get("Content-Type", "")
+        content_type = content_type.split(";", maxsplit=1)[0]
+        if content_type == "text/css":
+            response.cache_control.public = True
+            response.cache_control.immutable = True
+        return response
+
+    return app
