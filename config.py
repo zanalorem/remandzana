65d9771ffb2daf7b37f86cdbe7de2c7376cf0ecb
zanalorem <91803188+zanalorem@users.noreply.github.com>
2021-10-04 03:07:37 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 MAX_CONTENT_SIZE = 1024
-SECRET_KEY = NotImplemented
+SECRET_KEY = bytes.fromhex("b9462b5092694df9ba93fb4c539c1c4d397a01773f41e347d9157077d2358f9f")
 
 # enable the debug panopticon
 REMANDZANA_DEBUG = True
@@ -9,10 +9,14 @@ REMANDZANA_METRICS_PERIOD = 86400
 # period in which not to make new metrics calculations
 REMANDZANA_METRICS_COOLDOWN = 60
 
-# period for which an authentication key should last
+# period an authentication key should last for
 REMANDZANA_AUTH_TIMEOUT = 600
 # period in which not to generate a new authentication key
 REMANDZANA_AUTH_COOLDOWN = 10
 
 # directory to store feedback as JSON
 REMANDZANA_FEEDBACK_DIRECTORY = "feedback"
+
+# location of the Ahmia blacklist: https://ahmia.fi/blacklist/
+# used for the Ahmia censorship policy
+REMANDZANA_AHMIA_LOCATION = "ahmia.txt"
